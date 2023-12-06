64c7adf66e63eeadb198d41cb4ca7d10027f14fa
Oskari Lindroos <oskarilindroos00@gmail.com>
2023-12-06 16:26:52 +0200

    renamed state variable, removed placeholder image

--- react-dine-frontend/src/components/DishCard.jsx
+++ react-dine-frontend/src/components/DishCard.jsx
@@ -8,56 +8,49 @@ import {
   CheckCircleIcon,
   XCircleIcon,
 } from "@heroicons/react/24/solid";
-import placeholderImg from "../assets/dishPlaceholder.png";
 
 const imgBaseUrl = import.meta.env.VITE_API_BASE_URL.replace("/api", "");
 
 const DishCard = ({ dish }) => {
-  const [addedToCart, setAddedToCart] = useState(false);
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
   const { cart, addToCart, removeFromCart } = useContext(CartContext);
-
-  // For showing placeholder image until dish image is loaded
-  const onImageLoad = () => {
-    setImageLoaded(true);
-  };
 
   const handleAddToCart = () => {
     addToCart(dish);
-    setAddedToCart(true);
+    setIsAddedToCart(true);
     toast.success(`${dish.name} added to your order`);
   };
 
   const handleRemoveFromCart = () => {
     removeFromCart(dish.id);
-    setAddedToCart(false);
+    setIsAddedToCart(false);
     toast.info(`${dish.name} removed from your order`);
   };
 
   // Check if dish is already in cart when component is mounted
-  // and set addedToCart state accordingly
+  // and set isAddedToCart state accordingly
   useEffect(() => {
     const isDishInCart = cart.items.find((item) => item.id === dish.id);
-    setAddedToCart(isDishInCart);
+    setIsAddedToCart(isDishInCart);
   }, [cart, dish.id]);
 
   const getIcon = () => {
-    if (addedToCart) {
+    if (isAddedToCart) {
       return (
         <div>
-          <CheckCircleIcon className="w-12 h-12 text-green-400 group-hover:hidden" />
-          <XCircleIcon className="w-12 h-12 text-red-500 hover:text-red-600 cursor-pointer transition duration-300 ease-in-out hidden group-hover:block" />
+          <CheckCircleIcon className="h-12 w-12 text-green-400 group-hover:hidden" />
+          <XCircleIcon className="hidden h-12 w-12 cursor-pointer text-red-500 transition duration-300 ease-in-out hover:text-red-600 group-hover:block" />
         </div>
       );
     } else {
       return (
-        <PlusCircleIcon className="w-12 h-12 text-react-blue hover:text-green-400 cursor-pointer transition duration-300 ease-in-out" />
+        <PlusCircleIcon className="h-12 w-12 cursor-pointer text-react-blue transition duration-300 ease-in-out hover:text-green-400" />
       );
     }
   };
 
   const getCardHoverStyles = () => {
-    if (addedToCart) {
+    if (isAddedToCart) {
       return "-translate-y-2 outline outline-2 outline-green-400 hover:outline-red-400";
     } else {
       return "hover:-translate-y-2";
@@ -66,24 +59,23 @@ const DishCard = ({ dish }) => {
 
   return (
     <div
-      className="cursor-pointer group"
-      onClick={!addedToCart ? handleAddToCart : handleRemoveFromCart}
+      className="group cursor-pointer"
+      onClick={isAddedToCart ? handleRemoveFromCart : handleAddToCart}
     >
       <div
-        className={`flex flex-col max-w-md bg-ocean-light rounded-t-2xl  shadow-ocean-dark shadow-inner drop-shadow-md border-transparent hover:outline hover:outline-2 hover:outline-react-blue transition duration-300 ease-in-out ${getCardHoverStyles()} `}
+        className={`flex max-w-md flex-col rounded-t-2xl border-transparent  bg-ocean-light shadow-inner shadow-ocean-dark drop-shadow-md transition duration-300 ease-in-out hover:outline hover:outline-2 hover:outline-react-blue ${getCardHoverStyles()} `}
       >
         <img
-          className={`rounded-t-2xl border-b-2 min-h-full border-dotted pointer-events-none border-fire-dark bg-black`}
-          src={imageLoaded ? `${imgBaseUrl}/${dish.image}` : placeholderImg}
+          className={`pointer-events-none min-h-full rounded-t-2xl border-b-2 border-dotted border-fire-dark bg-black`}
+          src={`${imgBaseUrl}/${dish.image}`}
           alt={dish.name}
-          onLoad={onImageLoad}
         />
         <div className="flex flex-col gap-4 p-4">
           <div className="flex flex-col gap-2">
             <h2 className="text-xl">{dish.name}</h2>
             <p className="text-sm">{dish.description}</p>
           </div>
-          <div className="flex flex-row justify-between items-center gap-2">
+          <div className="flex flex-row items-center justify-between gap-2">
             <p className="text-xl font-bold">${dish.price}</p>
             {getIcon()}
           </div>
