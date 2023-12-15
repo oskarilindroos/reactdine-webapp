9439dd08efb760c4531893b1141518199a347f5d
Oskari Lindroos <oskarilindroos00@gmail.com>
2023-12-15 18:22:02 +0200

    fixed styles and animations

--- react-dine-frontend/src/pages/MenuPage.jsx
+++ react-dine-frontend/src/pages/MenuPage.jsx
@@ -18,20 +18,20 @@ const MenuPage = () => {
   return (
     <motion.div
       ref={menuRef}
-      className="flex flex-col gap-12 px-4  mx-auto my-8 justify-items-center max-w-6xl"
+      className="mx-auto my-8 flex max-w-6xl  flex-col justify-items-center gap-12 px-4"
       initial={{
         opacity: 0,
         y: 1000,
         transition: { duration: 2 },
       }}
-      animate={{ opacity: 1, y: 0, transition: { duration: 2 } }}
+      animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
       exit={{ opacity: 0, x: -1000, transition: { duration: 0.5 } }}
     >
       <ShoppingCartMenu />
-      <h1 className="text-3xl text-center my-8">What would you like today?</h1>
+      <h1 className="my-8 text-center text-3xl">What would you like today?</h1>
       {Boolean(loading) && <LoadingIcon />}
       {Boolean(error) && <p>{error.message ?? "An error occured."}</p>}
-      <section className="grid px-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <section className="grid grid-cols-1 gap-8 px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {data.map((dish) => (
           <motion.div
             key={dish.id}
